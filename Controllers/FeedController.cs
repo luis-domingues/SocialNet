f61ab55fa47b4f622c9e77b51e43d3777d93a9be
luis-domingues <luisdomingues117@gmail.com>
2024-10-16 18:03:23 -0300

    feat: add FeedController for managing user posts

--- Controllers/FeedController.cs
+++ Controllers/FeedController.cs
@@ -1,6 +1,51 @@
-﻿namespace SocialNet.Controllers;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialNet.Context;
+using SocialNet.Models;
 
-public class FeedController
+namespace SocialNet.Controllers;
+
+[Authorize]
+public class FeedController : Controller
 {
-    
+    private readonly SocialNetDbContext _context;
+
+    public FeedController(SocialNetDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var posts = _context.Posts
+            .Include(p => p.User)
+            .OrderByDescending(p => p.CreateAt)
+            .ToList();
+
+        return View(posts);
+    }
+        
+    public IActionResult CreatePost()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult CreatePost(Post post)
+    {
+        if (ModelState.IsValid)
+        {
+            var userId = int.Parse(User.Claims.FirstOrDefault(c => c.Type == "UserId")?.Value);
+            post.UserId = userId;
+
+            _context.Posts.Add(post);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        return View(post);
+    }
 }
