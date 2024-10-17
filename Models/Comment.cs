8fb808b8e161c4490afc072db17bbd702edfbf44
luis-domingues <luisdomingues117@gmail.com>
2024-10-17 17:44:01 -0300

    feat: add Comment model to store user comments on posts

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,6 +1,23 @@
-﻿namespace SocialNet.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SocialNet.Models;
 
 public class Comment
 {
+    public int Id { get; set; }
     
+    [Required]
+    [StringLength(200)]
+    public string Content { get; set; }
+
+    public DateTime CommentedAt { get; set; } = DateTime.Now;
+    
+    [ForeignKey("Post")]
+    public int PostId { get; set; }
+    public Post Post { get; set; }
+    
+    [ForeignKey("User")]
+    public int UserId { get; set; }
+    public User User { get; set; }
 }
