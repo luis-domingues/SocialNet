da8c6b87504d423e55fba0006c30a29ee7009f41
luis-domingues <luisdomingues117@gmail.com>
2024-10-21 16:19:48 -0300

    feat: add add follow entity to represent the follower

---
+++ Models/Follow.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SocialNet.Models;
+
+public class Follow
+{
+    public int Id { get; set; }
+    
+    [ForeignKey("Follower")]
+    public int FollowerId { get; set; }
+    public User Follower { get; set; }
+    
+    [ForeignKey("Followed")]
+    public int FollowedId { get; set; }
+    public User Followed { get; set; }
+}
