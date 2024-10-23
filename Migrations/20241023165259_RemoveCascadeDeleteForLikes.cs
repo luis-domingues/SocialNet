7a284763a8653bf388f638748ad5975d15387a9f
luis-domingues <luisdomingues117@gmail.com>
2024-10-23 14:52:01 -0300

    chore: configure migration for manual account deletion

---
+++ Migrations/20241023165259_RemoveCascadeDeleteForLikes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCascadeDeleteForLikes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
