fdfe97c4559d7b60d657a7f96e059c1614dafc95
luis-domingues <luisdomingues117@gmail.com>
2024-10-31 14:01:35 -0300

    fix: remove unnecessary console log in addNotificationToDropdown

--- wwwroot/js/notifications.js
+++ wwwroot/js/notifications.js
@@ -1,26 +1,15 @@
 ﻿const connection = new signalR.HubConnectionBuilder().withUrl("/notificationHub").build();
 
 connection.on("ReceiveNotification", function (message) {
-    console.log("Notificação recebida no frontend:", message);
     addNotificationToDropdown(message);
 });
 
-connection.start()
-    .then(() => {
-        console.log("Conectado ao SignalR com sucesso.");
-    })
-    .catch(function (err) {
-        console.error("Erro ao conectar ao SignalR:", err.toString());
-    });
+connection.start().catch(function (err) {
+    console.error("Erro ao conectar ao SignalR:", err.toString());
+});
 
 function addNotificationToDropdown(message) {
-    console.log("Tentando adicionar notificação ao dropdown:", message); // Log de verificação
-
     const notificationList = document.getElementById("notificationList");
-    if (!notificationList) {
-        console.error("Elemento notificationList não encontrado no DOM.");
-        return;
-    }
 
     if (notificationList.children[0] && notificationList.children[0].textContent === "Nenhuma notificação") {
         notificationList.innerHTML = "";
