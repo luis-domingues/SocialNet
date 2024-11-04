474c54963543133e477a1bcd91186bcabb22ef24
luis-domingues <luisdomingues117@gmail.com>
2024-11-04 13:34:16 -0300

    feat: configure cascading comment relationship in DbContext with ParentCommentId

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -20,4 +20,9 @@ public class Comment
     [ForeignKey("User")]
     public int UserId { get; set; }
     public User User { get; set; }
+    
+    public int? ParentCommentId { get; set; }
+    public Comment ParentComment { get; set; }
+    
+    public ICollection<Comment> Replies { get; set; }
 }
