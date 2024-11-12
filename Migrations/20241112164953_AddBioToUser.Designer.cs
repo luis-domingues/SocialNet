ea40dd0535bbaea8d74c297ce79b0ee469a56b10
luis-domingues <luisdomingues117@gmail.com>
2024-11-12 14:24:36 -0300

    migrations: create migration AddBioToUser to add bio field to users table

--- Migrations/20241112164953_AddBioToUser.Designer.cs
+++ Migrations/20241112164953_AddBioToUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SocialNet.Context;
 
@@ -11,9 +12,11 @@ using SocialNet.Context;
 namespace SocialNet.Migrations
 {
     [DbContext(typeof(SocialNetDbContext))]
-    partial class SocialNetDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241112164953_AddBioToUser")]
+    partial class AddBioToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
